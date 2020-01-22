27bed963dcc215cb722116cc3e865d6acb264392
Alfred Francis <alfred@cogniwide.com>
2020-01-22 19:26:14 +0530

    re- added uuid

--- src/ChatWidget/chatwidget.js
+++ src/ChatWidget/chatwidget.js
@@ -144,7 +144,17 @@ class ChatWidget extends Component {
 
 
   createOrRetriveSenderId() {
-    return "default"
+    return this.guid()
+  }
+
+  guid() {
+    function s4() {
+      return Math.floor((1 + Math.random()) * 0x10000)
+        .toString(16)
+        .substring(1);
+    }
+    return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
+      s4() + '-' + s4() + s4() + s4();
   }
 
   componentDidMount() {
