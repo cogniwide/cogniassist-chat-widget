5a92ae8ec28d601487592a2800914131e8d2b04e
Alfred Francis <alfred@cogniwide.com>
2020-02-18 14:06:59 +0530

    updated production lib

--- webpack.prod.js
+++ webpack.prod.js
@@ -25,10 +25,14 @@ module.exports = {
         loader: 'babel-loader'
       },
       {
-        test: /\.css$/,
+        test: /\.(css|scss)$/,
         use: [
-          { loader: 'style-loader' },
-          { loader: 'css-loader' }
+            // Creates `style` nodes from JS strings
+            'style-loader',
+            // Translates CSS into CommonJS
+            'css-loader',
+            // Compiles Sass to CSS
+            'sass-loader',
         ]
       },
       {
